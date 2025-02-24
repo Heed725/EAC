5662bc57e6f1e549cb32e98f711d1588aa95c023
Hemed Lungo <86722789+Heed725@users.noreply.github.com>
2025-02-24 14:04:46 +0300

    Update EAC.R

--- EAC.R
+++ EAC.R
@@ -1,18 +1,28 @@
-#***************************************************************************************************************************************#
-   #@725Hemed 
-    #EAC CODE 2 
-    #Due Before New year eve  
-#****************************************************************************************************************************************#
-#The Main Code
 library(rgeoboundaries)
- EAC_boundaries <- geoboundaries(c("Tanzania", "Kenya","Uganda","Rwanda","Burundi","South Sudan","Democratic republic of Congo"))
 library(ggplot2)
-  ggplot(data = EAC_boundaries) +
-   geom_sf() +
- #Label X and Y axis as Longitude and Latitude
-    xlab("Longitude") +
-    ylab("Latitude") + 
-#Title of Diagram
-    ggtitle("East africa community")
-  
-#*****************************************************************************************************************************************#
+library(dplyr)
+library(sf)
+
+# List of EAC countries including Somalia
+countries <- c("Tanzania", "Kenya", "Uganda", "Rwanda", "Burundi", "South Sudan", 
+               "Democratic Republic of Congo", "Somalia")
+
+# Fetch boundaries for each country and add a column with the country name
+EAC_boundaries <- bind_rows(lapply(countries, function(cntry) {
+  boundary <- geoboundaries(cntry)
+  boundary$country <- cntry  # Add country name column
+  return(boundary)
+}))
+
+# Plot using ggplot2 with different colors for each country
+ggplot(data = EAC_boundaries) +
+  geom_sf(aes(fill = country), color = "black") +  # Assign unique colors
+  scale_fill_manual(values = c("Tanzania" = "#1f78b4", "Kenya" = "#33a02c", "Uganda" = "#e31a1c", 
+                               "Rwanda" = "#ff7f00", "Burundi" = "#6a3d9a", "South Sudan" = "#b15928", 
+                               "Democratic Republic of Congo" = "#a6cee3", "Somalia" = "#b2df8a")) +  
+  xlab("Longitude") +
+  ylab("Latitude") + 
+  ggtitle("East Africa Community (Colorful Map)") +
+  theme_minimal() +  
+  theme(legend.title = element_blank())  # Remove legend title
+
